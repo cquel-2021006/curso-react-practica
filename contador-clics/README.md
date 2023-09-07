d3301e278447d85a60351500ec7dcbdc985419dd
unknown <cquel-2021006@kinal.edu.gt>
2023-09-07 08:39:52 -0600

    Subiendo Proyectos

---
+++ contador-clics/README.md
@@ -0,0 +1,11 @@
+# `npm init`
+
+Hará la descarga de la carpeta node_modules
+
+
+## `npm start`
+Ejecuta la aplicación en modo de desarrollo.
+Abra http://localhost:3000 para verlo en su navegador.
+
+La página se recargará cuando realice cambios.
+También puede ver errores de pelusa en la consola.
---
+++ contador-clics/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import Boton from './components/Boton';
+import Contador from './components/Contador';
+import jjlLogo from './imgs/jojolionlogo.png'
+import { useState } from 'react';
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
+  return (
+    <div className="App">
+      <div className='logo-contenedor'>
+        <img
+          className='Jojo'
+          src={jjlLogo}
+          alt='Jojolion Logo' />
+      </div>
+
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
